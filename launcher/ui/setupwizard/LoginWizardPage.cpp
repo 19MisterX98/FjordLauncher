2545aa8a5b12a9d1ae1cb7ff2d31d1146b219a21
misterx <weinboy11@gmail.com>
2025-09-24 15:13:03 +0200

    make authlib-injector the default

--- launcher/ui/setupwizard/LoginWizardPage.cpp
+++ launcher/ui/setupwizard/LoginWizardPage.cpp
@@ -4,6 +4,7 @@
 #include "ui_LoginWizardPage.h"
 
 #include "Application.h"
+#include "ui/dialogs/AuthlibInjectorLoginDialog.h"
 
 LoginWizardPage::LoginWizardPage(QWidget* parent) : BaseWizardPage(parent), ui(new Ui::LoginWizardPage)
 {
@@ -30,7 +31,10 @@ void LoginWizardPage::retranslate()
 void LoginWizardPage::on_pushButton_clicked()
 {
     wizard()->hide();
-    auto account = MSALoginDialog::newAccount(nullptr);
+    //auto account = MSALoginDialog::newAccount(nullptr);
+    auto account = AuthlibInjectorLoginDialog::newAccount(nullptr, tr("Please enter your username and password."
+                 "<br>"
+                 "<b>Caution!</b> Your username and password will be sent to the authentication server you specify!"));
     wizard()->show();
     if (account) {
         APPLICATION->accounts()->addAccount(account);
